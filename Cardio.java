cac0ded5279128cffe94119325f2bef5f53d48f9
pbyman <104075241+pbyman@users.noreply.github.com>
2022-04-20 09:56:06 -0400

    Cardio.java extends PlanX and added to cardio.txt

---
+++ Cardio.java
@@ -0,0 +1,19 @@
+public class Cardio extends PlanX{
+   
+   public Cardio(String n, int d) {
+      int sets;
+      int exs;
+      //ciruit = new ArrayList<String>();
+      int diff = d;
+      //String[] temp = readFile(n);   
+   
+   //so this will be harder to decide what to do with sets and exs bc there are not many excersies to do
+   //    d = 1  2  3  4  5  6  7  8  9  10
+   //  exs = 3  4  3  4  3  4  3  4  3  4
+   // sets = 
+   
+   }
+
+   //public ArrayList<String> start;
+
+}//end class
