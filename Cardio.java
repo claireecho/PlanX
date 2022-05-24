f8e8c711d947f9d00e1efd4f7874bc01c99071f2
pbyman <104075241+pbyman@users.noreply.github.com>
2022-05-24 17:21:00 -0400

    Add files via upload

--- Cardio.java
+++ Cardio.java
@@ -1,19 +1,32 @@
-public class Cardio extends PlanX{
+import java.util.*;
+
+public class Cardio extends PlanX
+{
    
-   public Cardio(String n, int d) {
-      int sets;
-      int exs;
-      //ciruit = new ArrayList<String>();
+   public Cardio(int d) throws IOException 
+   {
+      super(d, d % 2 == 0 ? d / 2 : d / 2 + 1, d % 2 == 0 ? 3 : 4);
       int diff = d;
-      //String[] temp = readFile(n);   
+      ArrayList<String> circuit = new ArrayList<String>();
+      String[] temp = readFile("Cardio.txt");
+      start(temp, d);
+   }
    
-   //so this will be harder to decide what to do with sets and exs bc there are not many excersies to do
+    public void start(String[] temp, int d) {
+      ArrayList<String> c = new ArrayList<String>();
+      w: while (c.size() < exercises) {
+            int ranEx = (int)(Math.random() * (temp.length) - 1);
+            for (int x = 0; x < c.size(); x++) {
+               if (temp[ranEx].equals(c.get(x)))
+                  continue w;
+            }
+         c.add(temp[ranEx]);
+      }
+      circuit = c;
+   }
+   
    //    d = 1  2  3  4  5  6  7  8  9  10
    //  exs = 3  4  3  4  3  4  3  4  3  4
-   // sets = 
+   // sets = 1  1  2  2  3  3  4  4  5  5 
    
-   }
-
-   //public ArrayList<String> start;
-
 }//end class
