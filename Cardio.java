2309ced941e7279a44239023c0c7c8ec180eb638
Claire <45241465+claireecho@users.noreply.github.com>
2022-05-27 21:09:53 -0400

    Finished Customize Workout Panel

--- Cardio.java
+++ Cardio.java
@@ -13,18 +13,29 @@ public class Cardio extends PlanX
       initate(temp, d);
    }
    
-    public void initate(String[] temp, int d) {
+   public void initate(String[] temp, int d) {
       ArrayList<String> c = new ArrayList<String>();
       w: while (c.size() < exercises) {
-            int ranEx = (int)(Math.random() * (temp.length) - 1);
-            for (int x = 0; x < c.size(); x++) {
-               if (temp[ranEx].equals(c.get(x)))
-                  continue w;
-            }
+         int ranEx = (int)(Math.random() * (temp.length) - 1);
+         for (int x = 0; x < c.size(); x++) {
+            if (temp[ranEx].equals(c.get(x)))
+               continue w;
+         }
          c.add(temp[ranEx]);
       }
       circuit = c;
-//add in the queue thing
+      for (String x : c)
+         queue.add(x);
+   }
+   
+   public String toString() 
+   {
+      String temp = "";
+      for (String x : circuit) 
+      {
+         temp += x + "-";
+      }
+      return temp;
    }
    
    //    d = 1  2  3  4  5  6  7  8  9  10
--- PlanX.java
+++ PlanX.java
@@ -11,6 +11,7 @@ public class PlanX {
    int sets;
    int exercises;
    
+   
    public PlanX(int d, int s, int ex) {
       diff = d;
       sets = s;
@@ -92,21 +93,37 @@ public class PlanX {
       }
    
    }
+   
+   
+   public static void main(String[] arg) throws IOException {
+      Abs circuit = new Abs (1); 
+      System.out.println("YOUR ABDOMINAL CIRCUIT\nsets: " + circuit.getSets());
+      
+      // make window
+      JFrame frame = new JFrame("PlanX");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setSize(500, 300);
+      frame.setVisible(true);
+      frame.setIconImage(new ImageIcon("icon.png").getImage());
+      
 
+   }
+   
 }
 
-class Helper extends TimerTask {
+class Helper extends TimerTask
+{
    private int i;
     
    public Helper(Integer g) {
       i = g;
    }
     
-   public void run() {
+   public void run()
+   {
       System.out.println("Timer ran " + i--);
       if (i == 0) {
          cancel();
       }
    }
 }
-
